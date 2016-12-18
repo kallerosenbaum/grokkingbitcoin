ad32d44771a5e5d252a178d094e6edc94fa10c70
Kalle Rosenbaum <kalle@rosenbaum.se>
2016-12-18 19:47:42 +0100

    Further revision and build script implification, ditching AAMakePDF for now.

--- buildbook.sh
+++ buildbook.sh
@@ -15,6 +15,7 @@ TEMPLATEDIR=${buildtooldir}/asciidoc-manning-templates
 
 mkdir -p ${outputdir}
 ln -sr ${sourcedir}/images ${outputdir}
+ln -sr ${sourcedir}/style ${outputdir}
 
 ## AAMakePDF must be run from the AAMakePDF dir
 ln -sr ${MAKEPDF} ${AAMAKEPDFDIR}
@@ -22,10 +23,13 @@ cp ${buildtooldir}/document.docbook45.erb ${TEMPLATEDIR}
 for file in ${sourcedir}/grokking-bitcoin.adoc; do
         basename=`echo $file | sed 's/.*\/\([^\/]*\)\.adoc/\1/'`
 	# This will generate a docbook xml file from the input
-	cat $file | asciidoctor -T ${TEMPLATEDIR} -b docbook45 - > ${outputdir}/${basename}.xml
+#	cat $file | asciidoctor -T ${TEMPLATEDIR} -b docbook45 - > ${outputdir}/${basename}.xml
+#	cat $file | asciidoctor -b docbook5 - > ${outputdir}/${basename}.xml
+	cat $file | asciidoctor -b html5 - > ${outputdir}/${basename}.html
+	cat $file | asciidoctor -r asciidoctor-pdf -b pdf - > ${outputdir}/${basename}.pdf
 
 	# This will create a .temp.xml file from the input .xml and
 	# generate a pdf from that temp file.
-	${AAMAKEPDFDIR}/makepdf.sh ${outputdir}/$basename.xml ${outputdir}/$basename.pdf
+#	${AAMAKEPDFDIR}/makepdf.sh ${outputdir}/$basename.xml ${outputdir}/$basename.pdf
 done
 rm ${scriptdir}/'c:\sw\text.txt'
