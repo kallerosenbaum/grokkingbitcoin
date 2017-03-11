36c922274b98e0ddce81ef3832d0733bf7c6e877
Kalle Rosenbaum <kalle@rosenbaum.se>
2017-03-11 15:04:32 +0100

    Added script to find unused images

---
+++ images/findunused.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+dir=`dirname $0`
+imagesdir=$dir
+docdir=$dir/..
+
+echo "Unused image files"
+for chdir in `find $imagesdir -type d -name "ch*"`; do
+    for file in `ls $imagesdir/$chdir`; do
+	grep -q "{imagedir}/$file\\[" $docdir/$chdir*.adoc
+	if [ $? != 0 ] ; then
+	    echo $chdir/$file
+	fi	
+    done
+done
+
+		
