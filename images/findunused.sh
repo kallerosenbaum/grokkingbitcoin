3912c927043548dd3bb8a970ef63caa969cd4a75
Kalle Rosenbaum <kalle@rosenbaum.se>
2018-06-14 09:47:28 +0200

    General cleanup
    * Removed unused images
    * Avoid reduntant directory name in docinfo.html
    * Removed unused css clauses
    * Stop blinking on "movingtarget" class
    * Improved findunused.sh to also include appendices

--- images/findunused.sh
+++ images/findunused.sh
@@ -5,7 +5,7 @@ imagesdir=$dir
 docdir=$dir/..
 
 echo "Unused image files"
-for chdir in `find $imagesdir -type d -name "ch*"`; do
+for chdir in `find $imagesdir -type d -and \( -name "app*" -or -name "ch*" \)`; do
     for file in `ls $imagesdir/$chdir`; do
 	grep -q "{imagedir}/$file\\[" $docdir/$chdir*.adoc
 	if [ $? != 0 ] ; then
