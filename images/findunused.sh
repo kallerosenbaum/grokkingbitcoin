9d1710cf17826c99278bdee4ce902442ede6e8b2
Kalle Rosenbaum <kalle@rosenbaum.se>
2019-04-13 23:17:11 +0200

    Exclude eps figures from findunused.sh

--- images/findunused.sh
+++ images/findunused.sh
@@ -6,7 +6,7 @@ docdir=$dir/..
 
 echo "Unused image files"
 for chdir in `find $imagesdir -type d -and \( -name "app*" -or -name "ch*" \)`; do
-    for file in `ls $imagesdir/$chdir`; do
+    for file in `ls $imagesdir/$chdir|grep -v '\.eps$'`; do
 	grep -q "{imagedir}/$file\\[" $docdir/$chdir*.adoc
 	if [ $? != 0 ] ; then
 	    echo $chdir/$file
